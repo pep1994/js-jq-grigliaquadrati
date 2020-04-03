b50205d3e39cd34e7968a9c4e19c6a9d1a6a3b83
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-03 19:13:26 +0200

    controllo click su quadrato già cliccato e animazione

--- main.js
+++ main.js
@@ -3,9 +3,10 @@
 $(document).ready(function(){
 
 
-var backSquare, numberSquare, greenScore = 0, redScore = 0;
+var backSquare, numberSquare, greenScore = 0, redScore = 0, scoreContent;
 
-
+$('#red-score').css({"color": "red"}); // coloro di rosso il punteggio red-score
+$('#green-score').css({"color": "green"}); // coloro di verde il punteggio green-score
 
   //  ciclo per stampare nell'html 64 div che rappresentano i quadrati
   for (var i = 1; i <= 64; i++) {
@@ -24,32 +25,56 @@ var backSquare, numberSquare, greenScore = 0, redScore = 0;
     }
 
   }
-  console.log(numberSquare.eq());
 
+  // modifico il valore della variabile in modo che punti a tutti i quadrati perchè dopo il ciclo il valore che rimane in memoria è solo l'ultimo generato
   numberSquare = $('.square');
 
+  // eseguo un ciclo for per aggiungere la class green a quei quadrati che non hanno la class red
   for (var j = 0; j < 64; j++) {
     if (!numberSquare.eq(j).hasClass('red')) {
       numberSquare.eq(j).addClass("green");
     }
   }
 
-  // modifico il valore della variabile in modo che punti a tutti i quadrati perchè dopo il ciclo il valore che rimane in memoria è solo l'ultimo generato
-
-  console.log(numberSquare.eq());
-
 
   // scateno l'evento click, quando si clicca su un quadrato con la class red si colorerà di rosso e aggiornerà il punteggio red-score, stessa cosa per i quadrati con la class green
   numberSquare.click(
     function() {
-      if ($(this).hasClass('red')) {
+
+      // se il quadrato ha la class "cliccato ccreo un messaggio per l'utente che lo avvisa che non può cliccare lo stesso quadrato più volte
+      if ($(this).hasClass('cliccato')) {
+        $('.modal-container').html('<div class="modal">' + '<h1>' + 'Non puoi cliccare più volte sullo stesso quadrato' + '</h1>' + '</div>'); // messaggio per l'utente
+        $('.modal').animate({ // animazione
+          opacity: 1,
+          top: 0,
+        }, 800, 'linear',
+        function () {
+          $('h1').css({"color": "darkorange"});
+        });
+
+        // altrimenti  quelli che sono stati cliccati con la class red si coloreranno di rosso e  scomparirà il messaggio per l'utente
+      } else if ($(this).hasClass('red')) {
         $(this).css({"background": "red"});
-        redScore++;
+        redScore++; // il punteggio viene incrementato
         $('#red-score').text("Red Score: " + (redScore));
+        $(this).addClass('cliccato'); // viene aggiunta la class cliccato in modo da non aumentare il punteggio se si riclicca sullo stesso quadrato
+        $('.modal').animate({ // animazione
+          opacity: 1,
+          top: '-100px',
+          rotate: '180deg'
+        }, 600);
+
+        // altrimenti  quelli che sono stati cliccati con la class green si coloreranno di verde e scomparirà il messaggio per l'utente
       } else if ($(this).hasClass('green')) {
         $(this).css({"background": "green"});
-        greenScore++;
+        greenScore++; // il punteggio viene incrementato
         $('#green-score').text("Green Score: " + (greenScore));
+        $(this).addClass('cliccato'); // viene aggiunta la class cliccato in modo da non aumentare il punteggio se si riclicca sullo stesso quadrato
+        $('.modal').animate({ // animazione
+          opacity: 1,
+          top: '-100px',
+          rotate: '180deg'
+        }, 600);
       }
   });
 
@@ -60,11 +85,5 @@ var backSquare, numberSquare, greenScore = 0, redScore = 0;
       return Math.floor(Math.random() * 64);
 
   }
-
-
-
-
-
-
 
 });
