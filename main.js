c9736b5ff3c11d99c45ab0ea6e8378529bd27d34
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-03 17:15:41 +0200

    al click campo minato random

--- main.js
+++ main.js
@@ -16,34 +16,45 @@ var backSquare, numberSquare;
   var number = 0;
   while (number < 15)  { // esegui il ciclio finchè non ci sono 15 numeri tutti diversi, cioè finchè la variabile di controllo è minore di 15
     numberSquare = $('.square').eq(numberRandom()); // seleziona l'elemento square all'interno dell'array
-    
+
     if (!numberSquare.hasClass('red')) { // se l'elemento selezionato non ha la class red
       numberSquare.addClass("red"); // allora aggiungila
       number++; // e incrementa la variabile di controllo di 1 e riesegui il ciclo
     }
 
   }
+  console.log(numberSquare.eq());
+
+  numberSquare = $('.square');
+
+  for (var j = 0; j < 64; j++) {
+    if (!numberSquare.eq(j).hasClass('red')) {
+      numberSquare.eq(j).addClass("green");
+    }
+  }
+
+  // modifico il valore della variabile in modo che punti a tutti i quadrati perchè dopo il ciclo il valore che rimane in memoria è solo l'ultimo generato
+
+  console.log(numberSquare);
+
+
+
+  numberSquare.click(
+    function() {
+      if ($(this).hasClass('red')) {
+        $(this).css({"background": "red"});
+      } else if ($(this).hasClass('green')) {
+        $(this).css({"background": "green"});
+      }
+  });
+
+
 
   // funzione per generare un numero random da 0 a 63
   function numberRandom () {
       return Math.floor(Math.random() * 64);
 
   }
-
-
-  // elementSquare.click(
-  //   function() {
-  //     if ($(this).hasClass('red')) {
-  //       $(this).css({"background": "red"});
-  //     } else if ($(this).hasClass('green')) {
-  //       $(this).css({"background": "green"});
-  //     }
-  // });
-
-
-
-
-
 
 
 
